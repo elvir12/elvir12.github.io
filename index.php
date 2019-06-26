89dc4cbc49d409fee0315bc101457523c149b8fb
vilipili <43701960+vilipili@users.noreply.github.com>
2019-06-26 19:21:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="login_style.css">
+</head>
+<body>
+
+<form action="loginform.php" method="post">
+  <input type="text" name="name" placeholder="FULL NAME">
+  <input type="text" name="mail" placeholder="Mail">
+  <input type="text" name="subject" placeholder="Subject">
+  <textarea name="message" placeholder="message"></textarea>
+  <button type="submit" name="submit">SEND MAIL</button>
+
+  
+</form>
+
+
+</body>
+</html>
