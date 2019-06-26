ef1c02bd45b94778bda4cba4b663856652699392
vilipili <43701960+vilipili@users.noreply.github.com>
2019-06-26 19:19:07 +0200

    test4

--- loginform.php
+++ loginform.php
@@ -1,14 +1,16 @@
 <?php  
 if (isset($_POST['submit'])) {
 	$name= $_POST['name'];
-	$password= $_POST['password'];
+	$subject= $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
 
 	$mailTo = "isoexauto@yahoo.com";
-	$headers= "FROM:".$name;
-	$txt = "New user request to sign in as:".$name.".\n\n\".$password;
+	$headers= "FROM:".$mailFrom;
+	$txt = "New user request to sign in as:".$name.".\n\n\".$message;
 	
 
 
-	mail($mailTo, $name, $txt, $headers);
+	mail($mailTo, $subject, $txt, $headers);
 	header("Location: log_in.html?mailsent");
 }
